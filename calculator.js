4c4fd1ebb9c15f8db2164c75d56c8ea855a2abc4
Kent Vincent Dela Concepcion <133948702+shiwooo@users.noreply.github.com>
2024-04-04 03:17:49 +0800

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $("#computebutton").click(function () {
+        var firstNum = $("#firstnum").val();
+        var secondNum = $("#secondnum").val();
+        var operation = $("#operations").val();
+
+        if (!firstNum || !secondNum || !operation) {
+            alert("Input all fields");
+            return
+        }
+
+        $.post('../Home/CalculatorOperation', {
+            frstNum: firstNum,
+            scndNum: secondNum,
+            op: operation
+
+        }, function (data) {
+            $("#answer").text(data[0].final_answer.toFixed(2));
+
+        });
+    });
+});
